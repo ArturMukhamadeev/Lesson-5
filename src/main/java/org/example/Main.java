562c7cc20cf1b5ab9109ddb68800687db26614d7
ArturMukhamadeev <praddartur@gmail.com>
2025-01-25 18:43:11 +0300

    Final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,9 @@ public class Main {
 
         employees[0] = new Employee("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "89231231212", 30000, 30);
         employees[1] = new Employee("Petrov Petr", "Manager", "petrov@mailbox.com", "89231231213", 40000, 35);
-        employees[2] = new Employee("Sidorov Sidr", "Developer", "sidorov@mailbox.com", "89231231214", 50000, 28);
-        employees[3] = new Employee("Kozlov Kozma", "Designer", "kozlov@mailbox.com", "89231231215", 35000, 40);
-        employees[4] = new Employee("Smirnov Smirna", "Analyst", "smirnov@mailbox.com", "89231231216", 45000, 32);
+        employees[2] = new Employee("Mukhamadeev Artur", "Developer", "sidorov@mailbox.com", "89231231214", 50000, 28);
+        employees[3] = new Employee("Kozlov Nikolay", "Designer", "kozlov@mailbox.com", "89231231215", 35000, 40);
+        employees[4] = new Employee("Sidorov Sergey", "Analyst", "smirnov@mailbox.com", "89231231216", 45000, 32);
 
         System.out.println("Информация о сотрудниках:");
         for (Employee employee : employees) {
